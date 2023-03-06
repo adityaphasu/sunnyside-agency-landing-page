e6009bbb1303f0483c661ffe556f25ac74ac8476
Aditya Phasu <adityaphasu919@gmail.com>
2023-03-06 21:12:45 +0530

    add custom hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -3,4 +3,5 @@ const navbarList = document.querySelector(".header__list");
 
 hamburgerIcon.addEventListener("click", () => {
   navbarList.classList.toggle("header__list--open");
+  hamburgerIcon.classList.toggle("navbar__hamburger--active");
 });
